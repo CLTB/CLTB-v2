68d06a84855a9e609d3ba54f2804871b36247b35
Yunzhi Huang <yunzhi.huang@gmail.com>
2016-09-28 21:44:46 -0700

    Add project files.

---
+++ CLTB/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\d3x632\documents\visual studio 2015\Projects\CLTB\CLTB\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
